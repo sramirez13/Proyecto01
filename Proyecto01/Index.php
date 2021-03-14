5a864769ca4cfdd51e863977703b1a6a245397ca
Sara Ramirez <sara.ramirez.a4@gmail.com>
2021-03-14 14:51:23 -0500

    Primer commit: Creando proyecto01

---
+++ Proyecto01/Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+</html lang="es">
+<head>
+    <title>Proyecto Heroku</title>
+    <meta charset="uth-8"/>
+    <link rel="stylesheet" content="text/css" href="css/normalize.css" />
+    <link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+<body>
+    <div id="envoltura"
+    <header>
+        <h1>Programaci&oacute;n web</h1>
+    </header>
+    <div id="main">
+        <p>
+            En este curso aprender&aacute;s a programar p&aacute;ginas web usando git.
+
+            <?php 
+                $curso = "Construcción de elementos de software 2";
+            echo "<br /><br />Estas en el curso: $curso ."   ?>
+        </p>
+    </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+</html lang="es">
+<head>
+    <title>Proyecto Heroku</title>
+    <meta charset="uth-8"/>
+    <link rel="stylesheet" content="text/css" href="css/normalize.css" />
+    <link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+<body>
+    <div id="envoltura"
+    <header>
+        <h1>Programaci&oacute;n web</h1>
+    </header>
+    <div id="main">
+        <p>
+            En este curso aprender&aacute;s a programar p&aacute;ginas web usando git.
+        </p>
+    </div>
+    </div>
+</body>
+</html>
